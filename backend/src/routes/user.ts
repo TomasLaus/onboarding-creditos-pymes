5b7b092258466a7cec7137534a934a658640418d
Miguel V.C <wmigue@gmail.com>
2025-10-02 10:59:07 -0300

    feat: validar datos antes de crear un nuevo usuario. chore: levantar el server de postgres y servidor express con npm run dev en el package.json con un solo comando.

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { create, getAll } from '../controllers/userController';
+import { createUserValidator } from '../middlewares/user-validator';
+import { validate } from '../middlewares/validatorRequest';
 
 const router = Router();
 
-router.post('/create', create);
+router.post('/create', createUserValidator, validate, create);
 router.get('/getAll', getAll);
 
 export default router;
