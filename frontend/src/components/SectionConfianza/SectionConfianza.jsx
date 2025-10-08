8f731bd0ca93ce57df4ddf79426c110c359636f5
Jhon Fredy Serna <jhonfserna@yahoo.com>
2025-10-07 20:31:39 -0500

    Agregué componentes SectionBeneficios y SectionConfianza

---
+++ frontend/src/components/SectionConfianza/SectionConfianza.jsx
@@ -0,0 +1,27 @@
+import './SectionConfianza.css';
+
+// Importación de imagenes:
+import logoSantander from '../../assets/01santander.png'; // Asegúrate de ajustar la ruta a tus imágenes
+import logoFalabella from '../../assets/02falabella.png';
+import logoBBVA from '../../assets/03bbva.png';
+import logoMacro from '../../assets/04macro.png';
+import logoSecure from '../../assets/05secure.png';
+
+const SectionConfianza = () => {
+  return (
+    <section className="section-confianza">
+      <h2 className="confianza-title">"Confianza y Respaldo"</h2>
+      <p className="confianza-subtitle">Respaldados por regulación SBS y aliados estratégicos</p>
+      <div className="logos-container">
+        <img src={logoSantander} alt="Logo Santander" className="logo" />
+        <img src={logoFalabella} alt="Logo Banco Falabella" className="logo" />
+        <img src={logoBBVA} alt="Logo BBVA" className="logo" />
+        <img src={logoMacro} alt="Logo Banco Macro" className="logo" />
+        <img src={logoSecure} alt="Logo Secure SSL Encryption" className="logo" />
+      </div>
+      <p className="confianza-footer">Más de 500 PYMES ya confían en nosotros</p>
+    </section>
+  );
+};
+
+export default SectionConfianza;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner_home from "../../components/Banner_home/Banner_home.jsx";
+import SectionConfianza from '../../components/SectionConfianza/SectionConfianza.jsx';
+import SectionBeneficios from "../../components/SectionBeneficios/SectionBeneficios.jsx";
 import "./Home.css";
 
 function Home() {
   return (
     <>
-      <Banner_home /> 
+      <Banner_home />
+      <SectionConfianza />
+      <SectionBeneficios /> 
     </>
   );
 }
