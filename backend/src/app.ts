6765ef84e0156d7a7b960b9ab1e21efa187ab9c9
Tomás Laus <tomaslaus99@gmail.com>
2025-10-09 23:59:18 -0300

    fix: nodemailer para produccion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,9 +10,9 @@ app.use(express.json())
 
 // Configuración de nodemailer (para activación de usuarios)
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: Number(process.env.SMTP_PORT) || 465,
-  secure: true, // true para TLS/SSL
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
