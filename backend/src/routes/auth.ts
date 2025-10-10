c7a55f5d0e96eecbedbc571f0d23065fc41270a2
Miguel V.C <wmigue@gmail.com>
2025-10-10 16:46:53 -0300

    merge: merge origin main

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,13 @@
-import { Router } from 'express';
-//import AuthController from '../controllers/authController';
+import { Router } from 'express'
+import { login, verify } from '../controllers/authController'
+import { forgotPassword, resetPassword } from '../controllers/authController'
 
-const router = Router();
-export default router;
+const router = Router()
+
+router.post('/login', login)
+router.get('/verify', verify)
+
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
+
+export default router
