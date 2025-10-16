e7409e2315b22891e7330d41eb868ac8dec6a0eb
Miguel V.C <wmigue@gmail.com>
2025-10-16 09:09:02 -0300

    fix: solucionar cors en render.

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { forgotPassword, resetPassword } from '../controllers/authController'
 
-
 const router = Router()
 
 router.post('/forgot-password', forgotPassword)
 router.post('/reset-password', resetPassword)
-
-
+router.get('/renderIsUpdate', (req, res) => {
+  res.json({ message: '10.15 am' })
+})
 
 export default router
--- backend/src/utils/enviar-email-activacion.ts
+++ backend/src/utils/enviar-email-activacion.ts
@@ -1,28 +1,26 @@
-import { transporter } from '../app'
+//ENVIO CON GMAIL nodemailer
 
-export function enviarEmailActivacion(
-  backend_url: string | undefined,
-  email: string,
-  token: string,
-  mensaje: string
-) {
-  // Generar link de activación
-  const activationLink = `http://${backend_url}/api/users/activate?token=${token}&email=${email}`
+// import { transporter } from '../app'
 
-  const mailOptions = {
-    from: process.env.SMTP_FROM || `"Soporte Onboarding PyMEs" <${process.env.SMTP_SENDER}>`, // remitente verificado
-    to: email,
-    subject: 'Activa tu cuenta',
-    text: `Haz click aquí para activar tu cuenta: ${activationLink}`,
-    html: `<a href="${activationLink}">${mensaje}</a>`
-  }
+// export function enviarEmailActivacion(backend_url: string | undefined, email: string, token: string, mensaje: string) {
+//   // Generar link de activación
+//   const activationLink = `http://${backend_url}/api/users/activate?token=${token}&email=${email}`
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('❌ Error enviando email de activación: ', error)
-      return
-    }
-    console.log(`📩 Email de activación enviado. Token: ${token}` , info.response)
-  })
-}
+//   const mailOptions = {
+//     from: process.env.SMTP_FROM || `"Soporte Onboarding PyMEs" <${process.env.SMTP_SENDER}>`, // remitente verificado
+//     to: email,
+//     subject: 'Activa tu cuenta',
+//     text: `Haz click aquí para activar tu cuenta: ${activationLink}`,
+//     html: `<a href="${activationLink}">${mensaje}</a>`
+//   }
 
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.error('❌ Error enviando email de activación: ', error)
+//       return
+//     }
+//     console.log(`📩 Email de activación enviado. Token: ${token}`, info.response)
+//   })
+// }
+
+//ENVIO CON BREVO EN PRODUCCION
