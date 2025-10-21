9db9ada58b89abc14a5f0ba910eb15bd7c8a40ff
Miguel V.C <wmigue@gmail.com>
2025-10-21 18:07:14 -0300

    fix: after merge appRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,27 +1,24 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom' // Importa Navigate
+import Home from '../pages/Home/Home'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop'
+import MainLayout from '../layouts/MainLayouts.jsx'
+import { useAppContext } from '../context/appContext'
 
-
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom"; // Importa Navigate
-import Home from "../pages/Home/Home";
-import RegisterPage from "../pages/RegisterPage/RegisterPage";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
-import MainLayout from "../layouts/MainLayouts.jsx";
-import { useAppContext } from "../context/appContext";
-
-
-import DashboardLayout from "../layouts/DashboardLayout/DashboardLayout";
-import PreparacionSolicitud from "../pages/PreparacionSolicitud/PreparacionSolicitud";
-import DashboardView from "../components/DashboardView/DashboardView.jsx";
-import SolicitudUno from "../pages/SolicitudUno/SolicitudUno.jsx";
+import DashboardLayout from '../layouts/DashboardLayout/DashboardLayout'
+import PreparacionSolicitud from '../pages/PreparacionSolicitud/PreparacionSolicitud'
+import DashboardView from '../components/DashboardView/DashboardView.jsx'
+import SolicitudUno from '../pages/SolicitudUno/SolicitudUno.jsx'
+import ActivarCuenta from '../pages/ActivarCuenta/ActivarCuenta.jsx'
 
 function AppRouter() {
-  const { tokenLogin } = useAppContext();
+  const { tokenLogin } = useAppContext()
   return (
     <>
       <ScrollToTop />
       <Routes>
-        
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route
@@ -32,26 +29,24 @@ function AppRouter() {
             path="/login"
             element={tokenLogin ? <DashboardLayout /> : <LoginPage />}
           />
+          <Route path="/activar-cuenta" element={<ActivarCuenta />} />
         </Route>
-
-// Rutas protegidas dentro del layout del dashboard //
-
+        <Route path="/activar-cuenta" element={<ActivarCuenta />} />
+        // Rutas protegidas dentro del layout del dashboard //
         <Route
           path="/dashboard"
-          element={tokenLogin ? <DashboardLayout /> : <Navigate to="/login"/>}
+          element={tokenLogin ? <DashboardLayout /> : <Navigate to="/login" />}
         >
-           {/* Rutas hijas */}
+          {/* Rutas hijas */}
           <Route index element={<DashboardView />} />
           <Route path="preparacion" element={<PreparacionSolicitud />} />
           <Route path="solicitud-uno" element={<SolicitudUno />} />
-       </Route>
-
-/////////////////////////////////////////////////////////////////
-
+        </Route>
+        /////////////////////////////////////////////////////////////////
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
-  );
+  )
 }
 
 export default AppRouter
