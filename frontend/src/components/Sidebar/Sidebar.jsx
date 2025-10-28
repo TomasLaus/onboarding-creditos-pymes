fdb396915639428913beac9dde0747b44de2d768
Miguel V.C <wmigue@gmail.com>
2025-10-28 19:28:44 -0300

    fix: actualizar boton cerrar sesion en /dashboard para que redirija a '/'

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -34,14 +34,24 @@ const Sidebar = ({ isOpen }) => {
       text: 'Estado de crédito',
       path: '/dashboard/estado-credito'
     },
-    { icon: <FiMessageSquare />, text: 'Chats', path: '/dashboard/chats', disabled: true },
-    { icon: <FiHelpCircle />, text: 'Ayuda', path: '/dashboard/ayuda', disabled: true }
+    {
+      icon: <FiMessageSquare />,
+      text: 'Chats',
+      path: '/dashboard/chats',
+      disabled: true
+    },
+    {
+      icon: <FiHelpCircle />,
+      text: 'Ayuda',
+      path: '/dashboard/ayuda',
+      disabled: true
+    }
   ]
 
   // Cierre de sesión
   const cerrarSesion = () => {
-    navigate('/')
-    setTokenLogin(null)
+    setTokenLogin(null) // Limpio el token primero
+    navigate('/', { replace: true }) // Luego voy a Home
   }
 
   return (
